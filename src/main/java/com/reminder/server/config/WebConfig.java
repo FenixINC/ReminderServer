c07e36edc25e3e0389979b1c2be2216810a4f57b
Taras Koloshmatin <taras@pier2marketing.com>
2018-07-25 19:10:40 +0300

    server

---
+++ src/main/java/com/reminder/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.reminder.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.reminder.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
